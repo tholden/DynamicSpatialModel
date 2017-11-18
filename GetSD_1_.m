beebbf5d6fc8a4d0cff1c1699de690e1d98c9a73
tholden <thomas.holden@gmail.com>
2017-11-18 11:47:36 +0000

    update initial values

--- GetSD_1_.m
+++ GetSD_1_.m
@@ -1,5 +1,5 @@
 function SD_1_ = GetSD_1_( psi1, psi2, psi3, N_1_LAG_, SN_1_, dBar, varargin )
-    SD_1_ = exp( fzero( @( log_SD_1_ ) GetResidual( exp( log_SD_1_ ), psi1, psi2, psi3, N_1_LAG_, SN_1_, dBar, varargin{:} ), -9, optimset( 'Display', 'iter' ) ) );
+    SD_1_ = exp( fzero( @( log_SD_1_ ) GetResidual( exp( log_SD_1_ ), psi1, psi2, psi3, N_1_LAG_, SN_1_, dBar, varargin{:} ), -8.2, optimset( 'Display', 'iter' ) ) );
 end
 
 function Residual = GetResidual( SD_1_, psi1, psi2, psi3, N_1_LAG_, SN_1_, dBar, varargin )
